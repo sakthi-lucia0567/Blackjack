36e740042bc204f271d26212f2e6142029a1c55a
Luci_Devil <sakthivel8725@outlook.com>
2022-08-17 10:16:09 +0530

    third commit

--- index.js
+++ index.js
@@ -4,36 +4,42 @@ let thirdCard;
 let sum;
 let hasBlackjack = false;
 let isAlive = true;
-let message = '';
-let messageEl = document.getElementById('message-el');
-let sumEl = document.getElementById('sum-el');
-let cardsEl = document.getElementById('cards-el');
+let message = "";
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
 
 firstCard = 22; //Math.floor(Math.random() * 12 + 2);
 secondCard = 0; //Math.floor(Math.random() * 12 + 2);
 thirdCard = 9;
 sum = firstCard + secondCard;
+takeThirdCard = firstCard + secondCard + thirdCard;
 console.log(firstCard);
 console.log(secondCard);
 console.log(sum);
 
 function startGame() {
-	if (sum === 21) {
-		message = "Wohoo..! you've got black jack🥳";
-		hasBlackjack = true;
-	} else if (sum < 21) {
-		message = 'Do u want to draw a new card.. ? 🤨';
-	} else {
-		message = "You're logging out of the game";
-		isAlive = false;
-	}
-	messageEl.innerText = message;
-	cardsEl.innerHTML = 'Cards : ' + firstCard + '   ' + secondCard;
-	sumEl.innerHTML = 'Sum : ' + sum;
-	console.log(message);
+  if (sum === 21) {
+    message = "Wohoo..! you've got black jack🥳";
+    hasBlackjack = true;
+  } else if (sum < 21) {
+    message = "Do u want to draw a new card.. ? 🤨";
+  } else {
+    message = "You're logging out of the game";
+    isAlive = false;
+  }
+  messageEl.innerText = message;
+  cardsEl.innerHTML = "Cards : " + firstCard + "   " + secondCard;
+  sumEl.innerHTML = "Sum : " + sum;
+  console.log(message);
 }
 
-function newCard() {}
+function newCard() {
+  cardsEl.innerHTML =
+    "Cards : " + firstCard + "   " + secondCard + " " + thirdCard;
+  sumEl.innerHTML = "Sum : " + takeThirdCard;
+  console.log(message);
+}
 
 // var age = window.prompt("Enter your age: ");
 // alert("Your name is " + age);
