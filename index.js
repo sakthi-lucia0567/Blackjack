5aa7977fc2192c703c0bf267425f4cd041a3f190
Luci_Devil <sakthivel8725@outlook.com>
2022-08-17 10:13:03 +0530

    second commit

--- index.js
+++ index.js
@@ -1,31 +1,39 @@
 let firstCard;
 let secondCard;
+let thirdCard;
 let sum;
 let hasBlackjack = false;
 let isAlive = true;
-let message = "";
+let message = '';
+let messageEl = document.getElementById('message-el');
+let sumEl = document.getElementById('sum-el');
+let cardsEl = document.getElementById('cards-el');
 
 firstCard = 22; //Math.floor(Math.random() * 12 + 2);
 secondCard = 0; //Math.floor(Math.random() * 12 + 2);
+thirdCard = 9;
 sum = firstCard + secondCard;
 console.log(firstCard);
 console.log(secondCard);
 console.log(sum);
 
-if (sum === 21) {
-    console.log("Wohoo..! you've got black jack🥳");
-    hasBlackjack = true;
-} else if (sum < 21) {
-    console.log("Do u want to draw a new card..? 🤨");
-} else {
-    message = "You're logging out of the game";
-    console.log(message);
-    isAlive = false;
+function startGame() {
+	if (sum === 21) {
+		message = "Wohoo..! you've got black jack🥳";
+		hasBlackjack = true;
+	} else if (sum < 21) {
+		message = 'Do u want to draw a new card.. ? 🤨';
+	} else {
+		message = "You're logging out of the game";
+		isAlive = false;
+	}
+	messageEl.innerText = message;
+	cardsEl.innerHTML = 'Cards : ' + firstCard + '   ' + secondCard;
+	sumEl.innerHTML = 'Sum : ' + sum;
+	console.log(message);
 }
 
-console.log(hasBlackjack);
-console.log(isAlive);
-console.log(message);
+function newCard() {}
 
 // var age = window.prompt("Enter your age: ");
 // alert("Your name is " + age);
